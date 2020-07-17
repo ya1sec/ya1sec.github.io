3b471003087262f1c2f022fa0b06ef383f967d35
unknown <theankitbagchi@gmail.com>
2020-07-16 21:32:03 -0400

    update with three pages

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let test = $("a");
 
 document.addEventListener("mousemove", function (event) {
+  $("#cube").removeClass("spinner-2");
   const x = event.pageX;
   const y = event.pageY;
 
@@ -16,33 +17,41 @@ document.addEventListener("mousemove", function (event) {
     "rotateX(" + midY * 0.5 + "deg) rotateY(" + midX * 0.5 + "deg)";
 });
 
-window.onload = function () {
-  //preload mouse down image here via Image()
-  $("#cube")
-    .bind("touchstart", function () {
-      $("#cube").attr("src", "button_on.png");
-      const x = event.pageX;
-      const y = event.pageY;
+// window.onload = function () {
+//   //preload mouse down image here via Image()
+//   $("#cube")
+//     .bind("touchstart", function () {
+//       $("#cube").attr("src", "button_on.png");
+//       const x = event.pageX;
+//       const y = event.pageY;
 
-      const midX = x - window.innerWidth / 2;
-      const midY = y - window.innerHeight / 2;
+//       const midX = x - window.innerWidth / 2;
+//       const midY = y - window.innerHeight / 2;
 
-      const box = document.querySelector("section");
+//       const box = document.querySelector("section");
 
-      box.style.left = x + "px";
-      box.style.top = y + "px";
+//       box.style.left = x + "px";
+//       box.style.top = y + "px";
 
-      box.style.transform =
-        "rotateX(" + midY * 0.5 + "deg) rotateY(" + midX * 0.5 + "deg)";
-    })
-    .bind("touchend", function () {});
-};
+//       box.style.transform =
+//         "rotateX(" + midY * 0.5 + "deg) rotateY(" + midX * 0.5 + "deg)";
+//     })
+//     .bind("touchend", function () {});
+// };
 
-$("#links").mouseenter(function () {
+$("nav").mouseenter(function () {
   $("#cube").addClass("hide");
 });
 
-$("#links").mouseleave(function () {
+$("nav").mouseleave(function () {
+  $("#cube").removeClass("hide");
+});
+
+$("p").mouseenter(function () {
+  $("#cube").addClass("hide");
+});
+
+$("p").mouseleave(function () {
   $("#cube").removeClass("hide");
 });
 
